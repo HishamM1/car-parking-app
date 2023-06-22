9e7fd4108c715d9d5d5eeb16a8f062e67dd38c77
HishamM1 <hishammedhat0@gmail.com>
2023-06-22 18:15:08 +0300

    Creating the register and home view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,6 +9,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/Auth/RegisterView.vue')
+    }
   ]
 })
 
